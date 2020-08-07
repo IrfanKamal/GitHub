f9600d286245996bc447663a8017f00d38c61d13
MIrfanKamal <irfan.kamal12@gmail.com>
2020-08-07 15:36:58 +0700

    Build and project

---
+++ Flappy Bird/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class Bullet : MonoBehaviour
+{
+    // Global variabel
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private Bird bird;
+    // Update is called once per frame
+    void Update()
+    {
+        //Melakukan pengecekan burung mati atau tidak
+        if (!bird.IsDead())
+        {
+            //menggerakan game object kesebelah kiri dengan kecepatan tertentu
+            transform.Translate(Vector3.right * bulletSpeed * Time.deltaTime, Space.World);
+        }
+    }
+
+    // Ketika menabrak pipa
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Mengecek apakah bertabrakan dengan pipa
+        if (collision.gameObject.CompareTag("Pipe"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/Shoot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Global variabel
+    [SerializeField] private Bullet bulletPrefab;
+    [SerializeField] private float reloadTime = 2f;
+    private bool bulletReady = true;
+    private Bird bird;
+    // Coroutine reload
+    Coroutine CR_reload;
+
+    private void Start()
+    {
+        bird = GetComponent<Bird>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(!bird.IsDead() && Input.GetMouseButtonDown(1) && bulletReady)
+        {
+            SpawnBullet();
+            bulletReady = false;
+            CR_reload = StartCoroutine(IeReload());
+        }
+    }
+
+    // Method me-spawn bullet
+    void SpawnBullet()
+    {
+        // Mengecek bullet
+        if (bulletPrefab != null)
+        {
+            Bullet bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator IeReload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        bulletReady = true;
+    }
+}
